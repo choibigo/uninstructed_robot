57666bcde3517342556578a4524227532ab2ad4a
hosung <hosunglee03@hanyang.ac.kr>
2024-02-13 16:10:02 +0900

    git test

--- src/omnigibson/hosung/my_map_robot_control_example_bin.py
+++ src/omnigibson/hosung/my_map_robot_control_example_bin.py
@@ -1,3 +1,18 @@
+#         # og.sim.viewer_camera.set_position_orientation(
+#         #     position=c_abs_pose,   # 
+#         #     orientation=c_abs_ori, # XYZW
+#         # )
+
+#         save_folder_path = os.path.join(save_root_path, f'{step}')
+#         os.makedirs(save_folder_path, exist_ok=True)
+#         cv2.imwrite(os.path.join(save_folder_path, 'rgb.png'), cv2.cvtColor(obs['robot0']['robot0:eyes_Camera_sensor_rgb'], cv2.COLOR_BGR2RGB))
+#         np.save(os.path.join(save_folder_path, 'depth.npy'), obs['robot0']['robot0:eyes_Camera_sensor_depth_linear'])
+#         np.save(os.path.join(save_folder_path, 'c_abs_pose.npy'), c_abs_pose)
+#         np.save(os.path.join(save_folder_path, 'c_abs_ori.npy'), c_abs_ori)
+#         np.save(os.path.join(save_folder_path, 'map_data_x.npy'), map_data_x)
+#         np.save(os.path.join(save_folder_path, 'map_data_y.npy'), map_data_y)
+#         np.save(os.path.join(save_folder_path, 'segmentation.npy'), segdata )
+
 """
 Example script demo'ing robot control.
 
@@ -17,52 +32,20 @@ import matplotlib.pyplot as plt
 
 
 import omnigibson as og
+import omni.replicator.core as rep 
+import omni.syntheticdata as sd
+
 from omnigibson.macros import gm
 from omnigibson.robots import REGISTERED_ROBOTS
 from omnigibson.utils.ui_utils import choose_from_options, KeyboardRobotController
 from omnigibson.utils.transform_utils import euler2quat, quat2euler, mat2euler, quat_multiply
 from omnigibson.sensors import scan_sensor
+# from omni.isaac.synthetic_utils.visualization import colorize_bboxes
 
-def quaternion_rotation_matrix(Q):
-    """
-    Covert a quaternion into a full three-dimensional rotation matrix.
- 
-    Input
-    :param Q: A 4 element array representing the quaternion (q0,q1,q2,q3) 
- 
-    Output
-    :return: A 3x3 element matrix representing the full 3D rotation matrix. 
-             This rotation matrix converts a point in the local reference 
-             frame to a point in the global reference frame.
-    """
-    # Extract the values from Q
-    q0 = Q[3]
-    q1 = Q[0]
-    q2 = Q[1]
-    q3 = Q[2]
+from utils import quaternion_rotation_matrix
 
-     
-    # First row of the rotation matrix
-    r00 = 2 * (q0 * q0 + q1 * q1) - 1
-    r01 = 2 * (q1 * q2 - q0 * q3)
-    r02 = 2 * (q1 * q3 + q0 * q2)
-     
-    # Second row of the rotation matrix
-    r10 = 2 * (q1 * q2 + q0 * q3)
-    r11 = 2 * (q0 * q0 + q2 * q2) - 1
-    r12 = 2 * (q2 * q3 - q0 * q1)
-     
-    # Third row of the rotation matrix
-    r20 = 2 * (q1 * q3 - q0 * q2)
-    r21 = 2 * (q2 * q3 + q0 * q1)
-    r22 = 2 * (q0 * q0 + q3 * q3) - 1
-     
-    # 3x3 rotation matrix
-    rot_matrix = np.array([[r00, r01, r02],
-                           [r10, r11, r12],
-                           [r20, r21, r22]])
-                            
-    return rot_matrix
+save_root_path = r"/home/bluepot/dw_workspace/git/uninstructed_robot/src/omnigibson/hosung/image_frames"
+save_folder_path = os.path.join(save_root_path, f'{0}')
 
 CONTROL_MODES = dict(
     random="Use autonomous random actions (default)",
@@ -78,6 +61,13 @@ SCENES = dict(
 gm.USE_GPU_DYNAMICS = False
 # gm.ENABLE_FLATCACHE = True
 gm.ENABLE_FLATCACHE=False
+
+def world_to_map(list_of_coor):
+    x_coor = list_of_coor[0]
+    y_coor = list_of_coor[1]
+    map_pixel_coor_x = (((y_coor / 5) * 512 ) + 511) // 1
+    map_pixel_coor_y = (((x_coor / 5) * 512 ) + 511) // 1
+    return (int(map_pixel_coor_x), int(map_pixel_coor_y))
 
 def choose_controllers(robot, random_selection=False):
     """
@@ -124,10 +114,9 @@ def main(random_selection=False, headless=False, short_exec=False):
 
     # Add the robot we want to load
     robot0_cfg = dict()
-    robot0_cfg["type"] = 'Turtlebot'
+    robot0_cfg["type"] = 'Locobot'
     robot0_cfg["obs_modalities"] = ["rgb", "depth_linear", "seg_instance"]
-    # robot0_cfg["obs_modalities"] = ["rgb", "depth", "scan", "occupancy_grid"]
-    # robot0_cfg["obs_modalities"] = ["rgb", "depth", "seg_instance", "normal", "scan", "occupancy_grid"]
+
 
     robot0_cfg["action_type"] = "continuous"
     robot0_cfg["action_normalize"] = True
@@ -137,8 +126,7 @@ def main(random_selection=False, headless=False, short_exec=False):
     # Compile config
     cfg = dict(scene=scene_cfg, robots=[robot0_cfg])
 
-    # Create the environment
-    ##############################################################################
+    # Create the environment - change the speed of action and physics
     env = og.Environment(configs=cfg, action_timestep=1/30., physics_timestep=1/30.)
 
     # Choose robot controller to use
@@ -181,32 +169,28 @@ def main(random_selection=False, headless=False, short_exec=False):
     horiz_aperture = env.robots[0].sensors['robot0:eyes_Camera_sensor'].get_attribute('horizontalAperture')
     vert_aperture = sensor_image_height/sensor_image_width * horiz_aperture
 
-
     focal_x = sensor_image_height * focal_length / vert_aperture
     focal_y = sensor_image_width * focal_length / horiz_aperture
     center_x = sensor_image_height * 0.5
     center_y = sensor_image_width * 0.5
 
-
-
     # Loop control until user quits
     max_steps = -1 if not short_exec else 100
     step = 0
 
-    map_data_x = np.array([])
-    map_data_y = np.array([])
-    segdata = np.array([])
-
-    save_root_path = r"/home/bluepot/dw_workspace/git/uninstructed_robot/src/omnigibson/hosung/image_frames"
-    os.makedirs(save_root_path, exist_ok=True)
-
     c_relative_pos, c_relative_ori = env.robots[0].sensors['robot0:eyes_Camera_sensor'].get_position_orientation()
 
     mapped_in_pixel = np.zeros([1024, 1024,3], dtype=np.uint8)
-    # mapped_in_pixel.fill([255, 255, 255])
     mapped_in_pixel[:, :, 0] = 255
     mapped_in_pixel[:, :, 1] = 255
     mapped_in_pixel[:, :, 2] = 255
+
+    object_synthetic_data = [[] for i in range(82)]
+
+    map_pixels = []
+    for i in range(60):
+        map_pixels = map_pixels + [[380, 200+10*i], [400, 200+10*i], [420, 200+10*i]] #, [440, 200+10*i], [460, 200+10*i]
+
 
     count = 0
     while step != max_steps:
@@ -216,45 +200,100 @@ def main(random_selection=False, headless=False, short_exec=False):
         agent_pos, agent_ori = env.robots[0].get_position_orientation()
         
         c_abs_pose, c_abs_ori = agent_pos + c_relative_pos, quat_multiply(agent_ori, c_relative_ori)
-        
-        # img = obs['robot0']['robot0:eyes_Camera_sensor_seg_instance']
-        # img = img.astype(np.uint8)
-        img2 = cv2.cvtColor(obs['robot0']['robot0:eyes_Camera_sensor_rgb'], cv2.COLOR_BGR2RGB)
-        img2[512][512] = [255,0,0]
-        cv2.imshow('img', mapped_in_pixel)
-        cv2.imshow('img2', img2)
-        cv2.waitKey(1)
 
+        cam = og.sim.viewer_camera
+        # cam.set_position_orientation(
+        # position=c_abs_pose,   # 
+        # orientation=c_abs_ori, # XYZW
+        # )
         
+        # seg_img = (obs['robot0']['robot0:eyes_Camera_sensor_seg_instance']).astype(np.uint8)
+        # cv2.imshow('seg', seg_img)
+
+        corners = []
+        bbox_modality = "bbox_3d"
+        cam.add_modality(bbox_modality)
+        obs2 = cam.get_obs()
+
         K = np.array([[focal_x,0, center_x, 0],
                       [0, focal_y, center_y, 0],
                       [0, 0, 1, 0]])
 
         K_inv = np.linalg.pinv(K)
 
-        # pixel_vertical = 262 #pixel_x
-        # pixel_horizontal = 131 #pixel_y
-        
+        if len(obs2[bbox_modality]) != 0:
+            
+            # if len(obs2[bbox_modality]) != 0:
+            # print((obs2[bbox_modality]))
+            for i in range(len(obs2[bbox_modality])): 
+                corners = obs2[bbox_modality][i][13]
+                if str(corners[0][0]) == 'nan':
+                    continue
+                else:
+                    corners = [world_to_map(corners[0]), 
+                                world_to_map(corners[1]), 
+                                world_to_map(corners[3]), 
+                                world_to_map(corners[2]), 
+                                world_to_map(corners[0])]
+                    mid_point = (int((corners[0][0]+corners[2][0])/2),
+                                 int((corners[0][1]+corners[2][1])/2))
+                    if not obs2[bbox_modality][i][2] in ['walls', 'ceilings', 'floors', 'agent']:
+                        # print(obs2[bbox_modality][i][0]-1)
+                        object_synthetic_data[obs2[bbox_modality][i][0]-1] = [obs2[bbox_modality][i][2], mid_point]
+                    # print("draw line")
+                    # if count == 0:
+                    #     for idx in range(4):
+                    #         cv2.line(mapped_in_pixel, corners[idx], corners[idx+1], (0,255,0))
+                    #     if not obs2[bbox_modality][i][2] in ['walls', 'ceilings', 'floors']:
+                    #         cv2.line(mapped_in_pixel, mid_point, mid_point, (0, 0, 0), 3)        
+                    #         cv2.putText(mapped_in_pixel,obs2[bbox_modality][i][2], mid_point, cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA )
+                        count = 1
 
-        map_pixels = [[400, 50*i] for i in range(21)]
+        list_of_seg_ids = []
+        # for i in range(0,1024*1024,30):
+        #     if obs['robot0']['robot0:eyes_Camera_sensor_seg_instance'].item(i) not in list_of_seg_ids:
+        #         list_of_seg_ids.append(obs['robot0']['robot0:eyes_Camera_sensor_seg_instance'].item(i))
+            
+        # list_of_seg_ids.sort()
+        # print(list_of_seg_ids)
 
-        # map_pixels = [[10, 100], [10, 200], [10, 300], [10, 400], [10, 500], [10, 600], [10, 700], [10, 800], [10, 900], [10, 1000]]
+
+
+        # image frame of what the camera is showing
+        # img2 = cv2.cvtColor(obs['robot0']['robot0:eyes_Camera_sensor_rjjjjjjjjgb'], cv2.COLOR_BGR2RGB) 
+
+        cv2.imshow('img', mapped_in_pixel)
+        # cv2.imshow('img2', img2)
+        cv2.waitKey(1)
+
         depth_map = obs['robot0']['robot0:eyes_Camera_sensor_depth_linear']
-        # depth_map[depth_map > 2] = 0
-        # depth_map[depth_map < 0.15] = 0
-        
-        # count += 1
+        segment_id_map = obs['robot0']['robot0:eyes_Camera_sensor_seg_instance']
+
         key = action_generator.current_keypress
         if str(key) == 'KeyboardInput.J' or str(key) == 'KeyboardInput.L' or str(key) == 'KeyboardInput.I' or str(key) == 'KeyboardInput.K':
+            # for object in list_of_seg_ids:
+            #     if not len(object_synthetic_data[object - 1]) == 0 and :
 
+            #         cv2.line(mapped_in_pixel, object_synthetic_data[object - 1][1], object_synthetic_data[object - 1][1], (0, 255, 0), 3)        
+            #         cv2.putText(mapped_in_pixel,object_synthetic_data[object - 1][0], object_synthetic_data[object - 1][1], cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,0), 1, cv2.LINE_AA )
+            
             for pixels in map_pixels:
                 pixel_vertical = pixels[0]
                 pixel_horizontal = pixels[1]
 
                 Zc = depth_map.item((pixel_vertical,pixel_horizontal))
-                if not 0.15 < Zc < 2 :
+                if not 0.15 < Zc < 2 : #< 2
                     continue
                 else:
+                    if segment_id_map.item((pixel_vertical, pixel_horizontal)) not in list_of_seg_ids:
+                        list_of_seg_ids.append(segment_id_map.item((pixel_vertical, pixel_horizontal)))
+                    list_of_seg_ids.sort()
+                    for object in list_of_seg_ids:
+                        if not len(object_synthetic_data[object - 1]) == 0 :
+
+                            cv2.line(mapped_in_pixel, object_synthetic_data[object - 1][1], object_synthetic_data[object - 1][1], (0, 255, 0), 3)        
+                            cv2.putText(mapped_in_pixel,object_synthetic_data[object - 1][0], object_synthetic_data[object - 1][1], cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,0), 1, cv2.LINE_AA )
+                    
                     scaled_coor = Zc * np.array([[pixel_horizontal], [pixel_vertical], [1]])
 
 
@@ -280,108 +319,18 @@ def main(random_selection=False, headless=False, short_exec=False):
 
                     extrinsic_callibration = np.matmul(RT_inv, intrinsic_callibration)
 
-
                     #code needed to be check on the following lines
                     extrinsic_callibration[0] += c_abs_pose[0]
                     extrinsic_callibration[1] += c_abs_pose[1]
                     extrinsic_callibration[2] += c_abs_pose[2]
 
+                    robot_in_pixel_coor_x = (((c_abs_pose[0].item() / 5) * 512 ) + 512) // 1
+                    robot_in_pixel_coor_y = (((c_abs_pose[1].item() / 5) * 512 ) + 512) // 1
+                    mapped_in_pixel[int(robot_in_pixel_coor_x),int(robot_in_pixel_coor_y), : ] = [0, 0, 255]
 
                     map_pixel_coor_x = (((extrinsic_callibration[0].item() / 5) * 512 ) + 512) // 1
                     map_pixel_coor_y = (((extrinsic_callibration[1].item() / 5) * 512 ) + 512) // 1
-
-                    robot_in_pixel_coor_x = (((c_abs_pose[0].item() / 5) * 512 ) + 512) // 1
-                    robot_in_pixel_coor_y = (((c_abs_pose[1].item() / 5) * 512 ) + 512) // 1
-
-
-
-
                     mapped_in_pixel[int(map_pixel_coor_x),int(map_pixel_coor_y), : ] = [255, 0, 0]
-                    mapped_in_pixel[int(robot_in_pixel_coor_x),int(robot_in_pixel_coor_y), : ] = [0, 0, 255]
-                    # mapped_in_pixel[:, :, 1] = 255
-                    # mapped_in_pixel[:, :, 2] = 255
-                    # mapped_in_pixel[int(map_pixel_coor_x)][int(map_pixel_coor_y)] = [0, 0, 255]
-                    # mapped_in_pixel[int(robot_in_pixel_coor_x)][int(robot_in_pixel_coor_y)] = [255, 0 ,0]
-
-
-
-
-                    # print(map_data.shape)
-                    # print(np.array([extrinsic_callibration[0],extrinsic_callibration[1]]).T.shape)
-                    # if  0.3 < extrinsic_callibration[2] < 0.35:
-                    #     map_data_x = np.concatenate((map_data_x, extrinsic_callibration[0]), axis=0)
-                    #     map_data_y = np.concatenate((map_data_y, extrinsic_callibration[1]), axis=0)
-                    #     segdata = np.concatenate((segdata, np.array([obs['robot0']['robot0:eyes_Camera_sensor_seg_instance'].item(pixel_vertical, pixel_horizontal)])))
-    
-            # count = 0
-
-        elif str(key) == 'KeyboardInput.P' : 
-            pixel_vertical = 512
-            pixel_horizontal = 512
-
-            Zc = depth_map.item((pixel_vertical,pixel_horizontal))
-            scaled_coor = Zc * np.array([[pixel_horizontal], [pixel_vertical], [1]])
-
-
-            rotation = quaternion_rotation_matrix(c_abs_ori)
-
-            x_vector = np.matmul(rotation, np.array([1,0,0]).T)
-            y_vector = np.matmul(rotation, np.array([0,-1,0]).T)
-            z_vector = np.matmul(rotation, np.array([0,0,-1]).T)
-
-            rotation_matrix = np.array([x_vector, y_vector, z_vector])
-
-            rotation_matrix_inv = np.linalg.inv(rotation_matrix)
-
-            transition_vector = -1 * np.matmul(rotation_matrix, c_abs_pose.T).T
-            
-            RT = np.concatenate((rotation_matrix, np.array([[transition_vector[0]],[transition_vector[1]],[transition_vector[2]]])), axis=1)
-            RT = np.concatenate((RT, np.array([[0, 0, 0, 1]])), axis=0)
-
-            RT_inv = np.concatenate((rotation_matrix_inv, np.array([[c_abs_pose[0]], [c_abs_pose[1]], [c_abs_pose[2]]])), axis=1)
-            RT_inv = np.concatenate((RT_inv, np.array([[0, 0, 0, 1]])), axis=0)
-
-            intrinsic_callibration = np.matmul(K_inv, scaled_coor)
-
-            extrinsic_callibration = np.matmul(RT_inv, intrinsic_callibration)
-
-            extrinsic_callibration[2] += c_abs_pose[2]
-            print()
-            print("Zc")
-            print(Zc)
-            print("agent_pos")
-            print(agent_pos)
-            print("relative_pos")
-            print(c_relative_pos)
-            print("c_abs_pos")
-            print(c_abs_pose)
-            print("world coor")
-            print(extrinsic_callibration)
-
-
-
-
-
-
-
-
-
-
-
-        # og.sim.viewer_camera.set_position_orientation(
-        #     position=c_abs_pose,   # 
-        #     orientation=c_abs_ori, # XYZW
-        # )
-
-        save_folder_path = os.path.join(save_root_path, f'{step}')
-        os.makedirs(save_folder_path, exist_ok=True)
-        cv2.imwrite(os.path.join(save_folder_path, 'rgb.png'), cv2.cvtColor(obs['robot0']['robot0:eyes_Camera_sensor_rgb'], cv2.COLOR_BGR2RGB))
-        np.save(os.path.join(save_folder_path, 'depth.npy'), obs['robot0']['robot0:eyes_Camera_sensor_depth_linear'])
-        np.save(os.path.join(save_folder_path, 'c_abs_pose.npy'), c_abs_pose)
-        np.save(os.path.join(save_folder_path, 'c_abs_ori.npy'), c_abs_ori)
-        np.save(os.path.join(save_folder_path, 'map_data_x.npy'), map_data_x)
-        np.save(os.path.join(save_folder_path, 'map_data_y.npy'), map_data_y)
-        np.save(os.path.join(save_folder_path, 'segmentation.npy'), segdata )
         
 
     # Always shut down the environment cleanly at the end
